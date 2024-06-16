676bfaa7c62b2a17cbd2dc30952c7f3b7e406861
Nik Shu <nik.shurenkov@mail.ru>
2024-06-16 23:55:39 +0300

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+     <div v-for="ch in characters" :key="ch.id">
+          <img :src="ch.image" alt="Character Image"/>
+          <h4><p>{{ ch.name }}-{{ ch.status }}</p></h4>
+      </div>
+  </div>
 </template>
 
+
+<script>
+  export default{
+      data(){
+          return {
+              characters:[]
+          };
+      },
+      created(){
+          this.fetchCharacters();
+      },
+      methods: {
+          async fetchCharacters(){
+              try {
+                  const response = await fetch('https://rickandmortyapi.com/api/character');
+                  const data = await response.json();
+                  this.characters = data.results;
+              }catch(error){
+                  console.error("Ошибка при получении данных от API",error);
+              }
+          }
+      }
+  };
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+ p{
+      color:blueviolet;
+      font-size: larger;
+      font-style: oblique 90deg;
+      
+ }
 </style>
